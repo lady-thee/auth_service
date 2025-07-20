3176ada2e6d574b9e2f4110ba384c8b1fbeb3919
Scarlet Lady <onuhtheola3@gmail.com>
2025-07-20 14:54:08 +0100

    Chore: Updated dtos with custom contract package

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable, Logger } from '@nestjs/common';
 // import { CreateUserDto } from './dto/register.dto';
